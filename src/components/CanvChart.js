fdd32bdc9700a54830247079c47c44962cb5e089
alexander yu <alexanderkyu27@gmail.com>
2021-05-03 21:25:28 -0400

    update first implementation of feeding data into the chart. Chart would not update because initial state does not change

--- src/components/CanvChart.js
+++ src/components/CanvChart.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 import Chart from "react-apexcharts";
 
+
+
 const CanvChart = ({ optionsArray }) => {
+
+    let arr = [];
+    const generate = () => {
+        arr = [];
+        if(optionsArray.length === 0){
+            // do nothing
+        }else{
+            for(var i = 0; i < optionsArray.length; i++){
+                let data = {x: parseInt(optionsArray[i].strikeprice), y: parseInt(optionsArray[i].strikeprice)};
+                arr.push(data);
+            }
+        }
+        return arr;
+    }
+
     const initialState = {
         options: {
           chart: {
@@ -22,37 +39,60 @@ const CanvChart = ({ optionsArray }) => {
           },
           series: [
               {
-              name: "series-1",
-              data:
-              
-              [ 
-                  {
-                      x: 1,
-                      y: 1,
-                  },{
-                      x: 2,
-                      y: 2
-                  },{
-                      x: 10,
-                      y: 10
-                  }
-              ]
+                data: arr
               }
-          ]
+            ]
           };
     const [ state, setState ] = useState(initialState)
 
+    generate();
+    console.log(arr);
     return (
         <div className="chart">
-                <Chart
-                    options={state.options}
-                    series={state.series}
-                    type="line"
-                    width="100%"
-                    height="400"
-                />
+            <Chart
+                options={state.options}
+                series={state.series}
+                type="line"
+                width="100%"
+                height="400"
+            />
         </div>
     )
 }
+
+
+
+// function renderGraph(arr){
+
+    
+//     let series = [
+//         {
+//         name: "series-1",
+//         data: [{
+//             x: 0,
+//             y: 0
+//         }]
+//         }
+//     ]
+
+
+//     if(arr.length === 0){
+//         state.updateSeries([{
+//             data: [{
+//                 x: 0,
+//                 y: 0
+//             }]
+//         }])
+//     }else{
+//         console.log("from render", arr[arr.length - 1].strikeprice);
+//         // CanvChart.updateSeries([{
+//         //     data: [{
+//         //         x: parseInt(arr[arr.length - 1].strikeprice),
+//         //         y: parseInt(arr[arr.length - 1].strikeprice)**2
+//         //     }]
+//         // }])
+//     }
+// }
+
 
 export default CanvChart
