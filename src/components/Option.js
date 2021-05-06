6f6966071e4f074872b3b464e6a44428b3069c4a
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-05-06 16:03:08 -0400

    removed form tag from Option

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,38 +2,36 @@ import React from 'react';
 
 const Option = ({num, handleChange}) => {
     return (
-        <div>
-            <form className="form" onSubmit={(e)=> {e.preventDefault()}}>
-                <div>
-                    Option {num}:
-                    <select  name="position" onChange={(event) => {handleChange(event,num)}}>
-                        <option name="long">Long</option>
-                        <option name="short">Short</option>
-                    </select>
+        <div className="form">
+            <div>
+                Option {num}:
+                <select  name="position" onChange={(event) => {handleChange(event,num)}}>
+                    <option name="long">Long</option>
+                    <option name="short">Short</option>
+                </select>
 
-                    <select name="type" onChange={(event) => {handleChange(event,num)}}>
-                        <option name="call">Call</option>
-                        <option name="put">Put</option>
-                    </select>
-                </div>
+                <select name="type" onChange={(event) => {handleChange(event,num)}}>
+                    <option name="call">Call</option>
+                    <option name="put">Put</option>
+                </select>
+            </div>
 
-                <label>
-                    Strike Price:  
-                    <input type="number" name="strikeprice"  onChange={(event) => {handleChange(event,num)}} placeholder={0}/>
-                </label>
+            <label>
+                Strike Price:  
+                <input type="number" name="strikeprice"  onChange={(event) => {handleChange(event,num)}} placeholder={0}/>
+            </label>
 
-                <label>
-                    Option Premium:  
-                    <input type="number" name="optionpremium"  onChange={(event) => {handleChange(event,num)}} placeholder={0}/>
-                </label>
-                <label>
-                    Contracts:  
-                    <input type="number" name="contracts"  onChange={(event) => {handleChange(event,num)}} placeholder={0}/>
-                </label>
-                <button className="remove">
-                    X
-                </button>
-            </form>
+            <label>
+                Option Premium:  
+                <input type="number" name="optionpremium"  onChange={(event) => {handleChange(event,num)}} placeholder={0}/>
+            </label>
+
+            <label>
+                Contracts:  
+                <input type="number" name="contracts"  onChange={(event) => {handleChange(event,num)}} placeholder={0}/>
+            </label>
+
+            <button className="remove">X</button>
         </div>
     )
 }
